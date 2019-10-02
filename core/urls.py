6229f9f70cb13f647974cec7636c28190e7e1249
Lorran Rosa <lorran@sixcreative.com.br>
2019-10-01 21:10:13 -0300

    New area add

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 
 from .views import (
     site_index,
-    user
+    user,
+    pre_game
 )
 
 
 urlpatterns = [
     path('', site_index.index, name='index'),
-    path('user/index', user.general.index, name="user_index")
+    path('user/index', user.general.index, name="user_index"),
+    path('play', pre_game.main.new_math_or_continue, name="new_or_continue")
 ]
